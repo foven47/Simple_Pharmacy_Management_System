395b64eab9b8e3e6941d27c84a07be9004576a1d
Pritom Mitra Pranto <91360837+PritomPranto@users.noreply.github.com>
2022-08-24 22:36:27 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ package pharmecy.ms;
 
 public class Main {
     public static void main(String[] args) {
-        PMS joy = new PMS();
-        joy.setVisible(true);
+        PMS pritom = new PMS();
+        pritom.setVisible(true);
     }
 
 }
